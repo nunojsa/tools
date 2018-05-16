3808a33e04ec52812356c271bcfbf694d57dd1f4
Nuno Sa <noname.nuno@gmail.com>
2018-05-16 23:44:27 +0200

    tools: Add dropbox script
    Simple script to start dropbox, wait for a specific file to sync and
    stop dropbox.
    
    Signed-off-by: Nuno Sa <noname.nuno@gmail.com>

---
+++ sync_db.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Simple script which starts dropbox, waits for a specific file to sync
+#and stops dropbox. It can be handy or things like setting triggers in
+#keepass for assuring syn on the database...
+
+function which_dropbox()
+{
+	 hash dropbox-cli 2>/dev/null  && echo "dropbox-cli" || { \
+	 hash caja-dropbox 2>/dev/null && echo "caja-dropbox"; }
+}
+
+#uncoment for debug
+#exec 15> $HOME/tools/log.txt
+#BASH_XTRACEFD=15
+#set -x
+
+dropbox=$(which_dropbox)
+
+[ -z "${dropbox}" ] && exit 0
+
+#set the file here...
+DB=$HOME/Dropbox/passwords/passwords.kdbx
+let cnt=0
+
+[ ! -f "${DB}" ] && exit 0
+
+##start dropbox
+${dropbox} start
+
+##check if DB is in sync. Sleep to give time for the sync process to start
+sleep 2
+while true; do
+	if [ "$(${dropbox} filestatus "${DB}" | awk -F": " '{print $2}')" == "up to date" ]; then
+		let cnt+=1
+		#let's give hysteresis of 3
+		[ ${cnt} -eq 3 ] && break
+	else
+		let cnt=0	
+	fi
+
+	sleep 1
+done
+
+#have your DB offline now
+${dropbox} stop
