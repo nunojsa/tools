a616ad3e060243fb8cfd44284ca8e928671f1e69
Nuno <noname.nuno@gmail.com>
2021-02-04 14:38:34 +0100

    vscode: add some tools for code
    It contains a simple script and a tasks.json file that I normally use
    for compile the linux kernel under code.
    
    Signed-off-by: Nuno <noname.nuno@gmail.com>

---
+++ vscode/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# this script is intended to build the linux kernel under vscode. It can be called
+# from tasks.json with the proper parameters. As I use this to mainly compile ADI
+# kernel, I try to find a proper Vivado installation to get the toolchain. However,
+# one can use any toolchain he wants as long as the proper env variables are passed.
+
+opt=""
+vivado=""
+version=$(cat Makefile | grep -e "VERSION *= *[0-9]" | cut -d "=" -f2 | tr -d " ")
+patchevel=$(cat Makefile | grep -e "PATCHLEVEL *= *[0-9]" | cut -d "=" -f2 | tr -d " ")
+full_ver="${version}.${patchevel}"
+
+# get vivado toolchains from the kernel version
+if [[ ${full_ver} == "4.19" ]]; then
+	vivado="$HOME/work/Vivado_2019_R1/Vivado/2019.1/settings64.sh"
+else
+	# default to the latest which is 5.4 kernel
+	vivado="$HOME/work/Vivado_2020_R1/Vivado/2020.1/settings64.sh"
+fi
+
+case "${1}" in
+"C=2")
+	# used for sparse
+	file=${2/.c/.o}
+	file=$(sed "s;${PWD}/;;" <<<${file})
+	opt="${1}"
+	;;
+"-d")
+	file=${2/.dts/.dtb}
+	if [[ ${arch} == "arm64" ]]; then
+		file=$(sed 's;.*boot/dts/;;' <<<${file})
+	else
+		file=$(basename ${file})
+	fi
+
+	# check for overlays...
+	[[ ${file} =~ .*-overlay.dtb ]] && file="overlays/"$(cut -d"-" -f1 <<<${file})".dtbo"
+	;;
+*)
+	opt="${1}"
+	;;
+esac
+
+[[ -f ${vivado} ]] && source ${vivado}
+make ARCH=${arch} CROSS_COMPILE=${cross_compile} ${opt} ${file} -j18
