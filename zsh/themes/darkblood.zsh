cd3621d820064ccf1a92cdb9685efcfda9fddc04
Nuno Sá <noname.nuno@gmail.com>
2026-06-15 15:28:46 +0100

    zsh: themes: darkblood: Add OS info
    Add os-release information which can be useful when remote working on
    some machine (so I can better distinguish where I am).
    
    While at it, remove the time given it only updated on enter which means
    it stalls pretty easily.
    
    Signed-off-by: Nuno Sá <noname.nuno@gmail.com>

--- zsh/themes/darkblood.zsh
+++ zsh/themes/darkblood.zsh
@@ -2,6 +2,13 @@
 autoload -U colors && colors
 setopt prompt_subst
 
-PROMPT=$'%{$fg[magenta]%}┌[%{$fg_bold[cyan]%}%D{%H:%M:%S}\%{$reset_color%}%{$fg[red]%}@%{$fg_bold[cyan]%}%D{%a,%b%d}%{$reset_color%}%{$fg[magenta]%}] %(?,,%{$fg[red]%}[%{$fg_bold[white]%}%?%{$reset_color%}%{$fg[red]%}])
+# Grab the OS name from /etc/os-release once (subshell keeps env clean).
+if [[ -r /etc/os-release ]]; then
+  PROMPT_OS="$(. /etc/os-release && echo "${ID:-$NAME}")"
+else
+  PROMPT_OS="unknown"
+fi
+
+PROMPT=$'%{$fg[magenta]%}┌(%{$fg_bold[green]%}'${PROMPT_OS}$'%{$reset_color%}%{$fg[magenta]%})[%{$fg_bold[cyan]%}%D{%a,%b%d}%{$reset_color%}%{$fg[magenta]%}] %(?,,%{$fg[red]%}[%{$fg_bold[white]%}%?%{$reset_color%}%{$fg[red]%}])
 %{$fg[magenta]%}└[%{$fg_bold[blue]%}%~%{$reset_color%}%{$fg[magenta]%}]%{$fg[red]%}$%{$reset_color%} '
 PS2=$' %{$fg[magenta]%}|>%{$reset_color%} '
