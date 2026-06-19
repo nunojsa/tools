3576735cf45a9368329da6728773639823bf0fb3
Nuno Sá <noname.nuno@gmail.com>
2026-06-19 15:25:46 +0100

    zsh: themes: darkblood: Make python venv does not break the prompt
    When activating a python virtualenv the prompt would become misaligned.
    Fix it by setting VIRTUAL_ENV_DISABLE_PROMPT=1 and handle the prompt
    ourselves.
    
    [Pretty much done by Opus 4.8]
    Signed-off-by: Nuno Sá <noname.nuno@gmail.com>

--- zsh/themes/darkblood.zsh
+++ zsh/themes/darkblood.zsh
@@ -2,6 +2,17 @@
 autoload -U colors && colors
 setopt prompt_subst
 
+# Render the venv segment ourselves instead of letting activate mangle PROMPT.
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+venv_prompt() {
+  [[ -n $VIRTUAL_ENV_PROMPT ]] || return
+  # VIRTUAL_ENV_PROMPT carries surrounding parens/space; strip them for the box.
+  local name=${${VIRTUAL_ENV_PROMPT## }%% }
+  name=${name#\(}; name=${name%\)}
+  print -n -- "[%{$fg_bold[yellow]%}${name}%{$reset_color%}%{$fg[magenta]%}]"
+}
+
 # Grab the OS name from /etc/os-release once (subshell keeps env clean).
 if [[ -r /etc/os-release ]]; then
   PROMPT_OS="$(. /etc/os-release && echo "${ID:-$NAME}")"
@@ -9,6 +20,7 @@ else
   PROMPT_OS="unknown"
 fi
 
-PROMPT=$'%{$fg[magenta]%}┌(%{$fg_bold[green]%}'${PROMPT_OS}$'%{$reset_color%}%{$fg[magenta]%})[%{$fg_bold[cyan]%}%D{%a,%b%d}%{$reset_color%}%{$fg[magenta]%}] %(?,,%{$fg[red]%}[%{$fg_bold[white]%}%?%{$reset_color%}%{$fg[red]%}])
+PROMPT=$'%{$fg[magenta]%}┌(%{$fg_bold[green]%}'${PROMPT_OS}$'%{$reset_color%}%{$fg[magenta]%})$(venv_prompt)[%{$fg_bold[cyan]%}%D{%a,%b%d}%{$reset_color%}%{$fg[magenta]%}] %(?,,%{$fg[red]%}[%{$fg_bold[white]%}%?%{$reset_color%}%{$fg[red]%}])
 %{$fg[magenta]%}└[%{$fg_bold[blue]%}%~%{$reset_color%}%{$fg[magenta]%}]%{$fg[red]%}$%{$reset_color%} '
 PS2=$' %{$fg[magenta]%}|>%{$reset_color%} '
+
